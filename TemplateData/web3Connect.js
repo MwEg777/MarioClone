10f3d3c2c7b69dd9ab1d39f2077053af35a9fd2f
Karim Salah <karemsalah97@gmail.com>
2021-04-26 09:41:34 +0200

    Add files via upload

---
+++ TemplateData/web3Connect.js
@@ -0,0 +1,47 @@
+//if (window.ethereum) {
+  //web3 = new Web3(window.ethereum);
+  // connect popup
+  //ethereum.enable();
+
+ // window.ethereum.on("accountsChanged", function () {
+  //  location.reload();
+  //});
+
+//}
+
+function ConnectAndGetWallet() {
+
+	var returnStr;
+
+	try {
+
+		if (window.ethereum) {
+
+			if (!web3)
+			{
+
+				web3 = new Web3(window.ethereum);
+
+
+			}
+
+			ethereum.enable();
+
+			returnStr = window.ethereum.selectedAddress;
+
+		}
+
+	}
+	catch (ee) {
+
+		window.alert("Couldn't connect Metamask. Please try again.");
+		returnStr = "";
+
+	}
+
+	var bufferSize = lengthBytesUTF8(returnStr) + 1;
+	var buffer = _malloc(bufferSize);
+	stringToUTF8(returnStr, buffer, bufferSize);
+	return buffer;
+
+}
